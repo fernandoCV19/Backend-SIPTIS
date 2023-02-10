0f83049dd42420ef9fdc688765f7444e73704086
fernandoCV19 <fernandocuva@gmail.com>
2023-02-10 11:50:52 -0400

    first commit

---
+++ src/test/java/BackendSIPTIS/BackendSiptisApplicationTests.java
@@ -0,0 +1,13 @@
+package BackendSIPTIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSiptisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
