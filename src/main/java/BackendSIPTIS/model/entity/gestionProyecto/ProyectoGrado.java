8243f6266be7ca959ac4754b2f5c23d0b3c64e62
fernandoCV19 <fernandocuva@gmail.com>
2023-02-16 13:28:48 -0400

    Relaciones de la entidad proyecto

--- src/main/java/BackendSIPTIS/model/entity/gestionProyecto/ProyectoGrado.java
+++ src/main/java/BackendSIPTIS/model/entity/gestionProyecto/ProyectoGrado.java
@@ -1,7 +1,10 @@
 package BackendSIPTIS.model.entity.gestionProyecto;
 
+import BackendSIPTIS.model.entity.editoresYRevisores.*;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "proyecto_grado")
@@ -21,4 +24,41 @@ public class ProyectoGrado {
     private String dirLibroAzul;
     private String dirProyecto;
     private String fase;
+
+    @OneToOne
+    private Defensa defensa;
+
+    @ManyToOne
+    private Modalidad modalidad;
+
+    @ManyToMany
+    private Collection<SubArea> subAreas;
+
+    @ManyToMany
+    private Collection<AreaProyecto> areas;
+
+    @OneToMany
+    private Collection<Presentacion> presentaciones;
+
+    @ManyToOne
+    private Estado estado;
+
+    @OneToMany
+    private Collection<Actividad> actividades;
+
+    @OneToMany
+    private Collection<EstudianteProyecto> estudiantes;
+
+    @OneToMany
+    private Collection<SupervisorProyecto> supervisores;
+
+    @OneToMany
+    private Collection<TutorProyecto> tutores;
+
+    @OneToMany
+    private Collection<DocenteTG2Proyecto> docentes;
+
+    @OneToMany
+    private Collection<TribunalProyecto> tribunales;
+
 }
