531fb68d7c288b29ffca829ee9cf12efd8da7b96
Alejandro20032001 <dilanantezama@gmail.com>
2023-02-26 19:18:33 -0400

    new entity for general activities

---
+++ src/main/java/BackendSIPTIS/model/entity/gestionProyecto/ActividadGeneral.java
@@ -0,0 +1,27 @@
+package BackendSIPTIS.model.entity.gestionProyecto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "actividad_general")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActividadGeneral {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "descripcion_actividad")
+    private String descripcionActividad;
+    @Column(name = "fecha_actividad")
+    private Date fechaActividad;
+    @Column(name = "recordatorio_wpp")
+    private String recordatorioWpp;
+}
