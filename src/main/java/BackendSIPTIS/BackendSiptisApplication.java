73ef34b66f03494e1b1cb62fdf880469e734506c
Alejandro20032001 <dilanantezama@gmail.com>
2023-02-26 20:14:40 -0400

    Some configuration for schedule

--- src/main/java/BackendSIPTIS/BackendSiptisApplication.java
+++ src/main/java/BackendSIPTIS/BackendSiptisApplication.java
@@ -2,8 +2,10 @@ package BackendSIPTIS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendSiptisApplication {
 
 	public static void main(String[] args) {
