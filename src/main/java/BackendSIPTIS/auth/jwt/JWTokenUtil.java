7a027aaefa4d178fcad75b9e0ee496545860b5f8
mavl44 <maury.vargasl@gmail.com>
2023-03-05 18:37:35 -0400

    creacion de carpeta jwt y clase JWTokenUtil, con tiempo de expiracion y accesstoken

---
+++ src/main/java/BackendSIPTIS/auth/jwt/JWTokenUtil.java
@@ -0,0 +1,11 @@
+package BackendSIPTIS.auth.jwt;
+
+public class JWTokenUtil {
+
+    private static final long EXPIRE_TIME_DURATION = 2 * 60 * 60* 1000; //2horas.
+    private static final String ACCESS_TOKEN_SECRET= "abcdefghijklmnOPQRSTUVWXYZ";
+
+    
+
+
+}
