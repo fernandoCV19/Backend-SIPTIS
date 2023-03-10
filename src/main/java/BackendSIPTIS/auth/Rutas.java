8cb80ca4abdd4e3696d3c98fe3d074b04c774d33
mavl44 <maury.vargasl@gmail.com>
2023-03-10 18:03:13 -0400

    creando rutas de prueba

--- src/main/java/BackendSIPTIS/auth/Rutas.java
+++ src/main/java/BackendSIPTIS/auth/Rutas.java
@@ -1,7 +1,23 @@
 package BackendSIPTIS.auth;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Rutas {
+
+    @GetMapping("/hola")
+    public String verTodos(){
+        return "Hola mundo";
+    }
+
+    @GetMapping("/editor")
+    public String soloEditor(){
+        return "hola editor";
+    }
+
+    @GetMapping("/admin")
+    public String soloAdmin(){
+        return "hola admin";
+    }
 }
