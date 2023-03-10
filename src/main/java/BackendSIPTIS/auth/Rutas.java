0a6e4507eaef1c54d1a831acd2b6c202f465d93f
mavl44 <maury.vargasl@gmail.com>
2023-03-10 18:18:34 -0400

    asignando roles a las rutas

--- src/main/java/BackendSIPTIS/auth/Rutas.java
+++ src/main/java/BackendSIPTIS/auth/Rutas.java
@@ -1,5 +1,6 @@
 package BackendSIPTIS.auth;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +13,13 @@ public class Rutas {
     }
 
     @GetMapping("/editor")
+    @PreAuthorize("hasAuthority('EDITOR')")
     public String soloEditor(){
         return "hola editor";
     }
 
     @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String soloAdmin(){
         return "hola admin";
     }
