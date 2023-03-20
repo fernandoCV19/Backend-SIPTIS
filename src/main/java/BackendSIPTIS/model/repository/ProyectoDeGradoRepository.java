27b3c2f1f9029b06c047dce65febeb44d666efac
pabloskick <darkour.m@gmail.com>
2023-03-20 16:28:23 -0400

    Implementacion parcial para la entidad y rutas de PROYECTODEGRADO

--- src/main/java/BackendSIPTIS/model/repository/ProyectoDeGradoRepository.java
+++ src/main/java/BackendSIPTIS/model/repository/ProyectoDeGradoRepository.java
@@ -1,7 +1,10 @@
 package BackendSIPTIS.model.repository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
+import BackendSIPTIS.model.entity.gestionProyecto.Presentacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +12,6 @@ import BackendSIPTIS.model.entity.gestionProyecto.ProyectoGrado;
 
 @Repository
 public interface ProyectoDeGradoRepository extends JpaRepository<ProyectoGrado, Long> {
-    ProyectoGrado findById(int id);
-    List <ProyectoGrado> findAll();
-    //void deleteById(int id);
+    Optional<ProyectoGrado> findById(int id);
 
-    
 }
---
+++ src/main/java/BackendSIPTIS/service/ProyectoGradoService.java
@@ -0,0 +1,32 @@
+package BackendSIPTIS.service;
+
+import BackendSIPTIS.commons.MensajeServicio;
+import BackendSIPTIS.commons.RespuestaServicio;
+import BackendSIPTIS.model.entity.gestionProyecto.Presentacion;
+import BackendSIPTIS.model.entity.gestionProyecto.ProyectoGrado;
+import BackendSIPTIS.model.repository.ProyectoDeGradoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProyectoGradoService {
+    @Autowired
+    private ProyectoDeGradoRepository proyectoDeGradoRepository;
+
+    public RespuestaServicio obtenerPresentaciones (Long idProyecto){
+        Optional<ProyectoGrado> oProyectoGrado = proyectoDeGradoRepository.findById(idProyecto);
+        if(oProyectoGrado.isEmpty()){
+            return RespuestaServicio.builder().mensajeServicio(MensajeServicio.NOT_FOUND).data(null).build();
+        }
+        ProyectoGrado  proyecto = oProyectoGrado.get();
+        List<Presentacion> presentaciones = proyecto.getPresentaciones().stream().toList();
+        if (presentaciones.size()==0){
+            return RespuestaServicio.builder().mensajeServicio(MensajeServicio.SIN_PRESENTACIONES).data(null).build();
+        }
+        return RespuestaServicio.builder().mensajeServicio(MensajeServicio.OK).data(presentaciones).build();
+    }
+
+}
