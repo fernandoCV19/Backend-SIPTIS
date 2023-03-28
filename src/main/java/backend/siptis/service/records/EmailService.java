e3a04b7c8ccfaa4f9826903b76edf579e3733a69
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-28 10:41:28 -0400

    service for sender email

---
+++ src/main/java/backend/siptis/service/records/EmailService.java
@@ -0,0 +1,23 @@
+package backend.siptis.service.records;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(String from, String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
