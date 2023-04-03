378cd51407fba689403c0f0cdf60f2f0139e4ede
Alejandro20032001 <dilanantezama@gmail.com>
2023-04-03 11:56:50 -0400

    missing fields added on activityDTO

--- src/main/java/backend/siptis/model/pjo/dto/records/ActivityDTO.java
+++ src/main/java/backend/siptis/model/pjo/dto/records/ActivityDTO.java
@@ -8,6 +8,8 @@ import java.util.Date;
 @Getter
 @Setter
 public class ActivityDTO {
+    private String activityName;
     private String activityDescription;
     private Date activityDate;
+    private long idProject;
 }
