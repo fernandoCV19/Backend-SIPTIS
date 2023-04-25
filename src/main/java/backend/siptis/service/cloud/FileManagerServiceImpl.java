ccf4c633a397ce1f1b73e7a659bc381abfc58eb2
pabloskick <darkour.m@gmail.com>
2023-04-24 21:57:55 -0400

    file manager service for easy access (download)

---
+++ src/main/java/backend/siptis/service/cloud/FileManagerServiceImpl.java
@@ -0,0 +1,29 @@
+package backend.siptis.service.cloud;
+
+import backend.siptis.commons.ServiceAnswer;
+import backend.siptis.commons.ServiceMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+@Service
+@RequiredArgsConstructor
+public class FileManagerServiceImpl implements FileManagerService{
+    @Autowired
+    private CloudManagementService nube;
+
+    @Override
+    public ServiceAnswer downloadFileFromCloud(String key){
+        ByteArrayOutputStream response= nube.getObject(key);
+        if (response == null){
+            return ServiceAnswer.builder().serviceMessage(ServiceMessage.NOT_FOUND).data(null).build();
+        }
+        return ServiceAnswer.builder().serviceMessage(ServiceMessage.OK).data(response).build();
+
+    }
+
+
+}
