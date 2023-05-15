655acec2781123decfa52df26e9a61ddbc2ff37b
Alejandro20032001 <dilanantezama@gmail.com>
2023-05-15 17:53:26 -0400

    query to find all personal activities added

--- src/main/java/backend/siptis/model/repository/userData/SiptisUserRepository.java
+++ src/main/java/backend/siptis/model/repository/userData/SiptisUserRepository.java
@@ -2,7 +2,11 @@ package backend.siptis.model.repository.userData;
 
 import backend.siptis.auth.entity.SiptisUser;
 import backend.siptis.model.entity.projectManagement.Project;
+import backend.siptis.model.entity.records.Activity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -27,5 +31,7 @@ public interface SiptisUserRepository extends JpaRepository<SiptisUser, Long> {
     boolean existsByTokenPassword(String tokenPassword);
 
     Optional<SiptisUser> findById(Long id);
-    Optional <Project> findProjectById(Long id);
+    @Query("SELECT a FROM Activity a, ProjectStudent ps " +
+            "WHERE ps.student.id = :id AND ps.project.id = a.project.id")
+    Page<Activity> findAllPersonalActivities(Long id, Pageable pageable);
 }
