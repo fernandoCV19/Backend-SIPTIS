530c312a6e3a32ab12e90724380e26829d67a7b4
Alejandro20032001 <dilanantezama@gmail.com>
2023-05-26 01:26:47 -0400

    phase entity created

---
+++ src/main/java/backend/siptis/model/entity/projectManagement/Phase.java
@@ -0,0 +1,27 @@
+package backend.siptis.model.entity.projectManagement;
+
+import backend.siptis.model.entity.userData.Document;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "phase")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Phase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "phase", fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Collection<Document> documents;
+}
