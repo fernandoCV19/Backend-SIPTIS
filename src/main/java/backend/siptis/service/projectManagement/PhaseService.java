0ddf4bc225105c6d30f267fe505228b1f76e2017
Alejandro20032001 <dilanantezama@gmail.com>
2023-05-30 23:52:35 -0400

    services for phases

---
+++ src/main/java/backend/siptis/service/projectManagement/PhaseService.java
@@ -0,0 +1,12 @@
+package backend.siptis.service.projectManagement;
+
+import backend.siptis.commons.ServiceAnswer;
+import backend.siptis.model.pjo.dto.PhaseDTO;
+
+public interface PhaseService {
+    ServiceAnswer createPhase (PhaseDTO phaseDTO);
+    ServiceAnswer deletePhase (Long idPhase);
+    ServiceAnswer updatePhase (PhaseDTO phaseDTO, Long idPhase);
+    ServiceAnswer findAllPhases ();
+    ServiceAnswer findPhaseById (Long idPhase);
+}
