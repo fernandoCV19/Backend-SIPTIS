6dbe2671b702a78e427bf6695b4790a5bdc74672
mavl44 <maury.vargasl@gmail.com>
2023-11-19 00:31:45 -0400

    email service

---
+++ src/main/java/backend/siptis/service/notifications/EmailService.java
@@ -0,0 +1,27 @@
+package backend.siptis.service.notifications;
+
+import backend.siptis.commons.ServiceAnswer;
+import backend.siptis.model.pjo.dto.userDataDTO.ChangePasswordDTO;
+import backend.siptis.model.pjo.dto.userDataDTO.TokenPasswordDTO;
+import jakarta.mail.Address;
+import jakarta.mail.MessagingException;
+
+import java.io.IOException;
+import java.util.Date;
+
+public interface EmailService {
+    void sendGeneralActivities() throws MessagingException, IOException;
+
+    ServiceAnswer changePassword(TokenPasswordDTO tokenPasswordDTO);
+
+    String readFile(String filen) throws IOException;
+
+    void sendEmailFromTemplate(Address[] arrayTO, String activityName, String activityDescription,
+                               Date activityDate) throws MessagingException, IOException;
+
+    void sendSpecificEmail(String email, String messageNotification) throws MessagingException, IOException;
+
+    ServiceAnswer sendRecoverPasswordEmail(String email) throws MessagingException, IOException;
+
+    ChangePasswordDTO createChangePasswordDTO(String email);
+}
